88604e988f025d0b0a4f97ed95393f8b2871f7a0
Anais Zulfequar <zulfeqan@sheridancollege.ca>
2024-01-30 00:06:14 -0500

    Add project files.

---
+++ AlgorithmsDataStructures/ADT/Pokemon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsDataStructures.ADT
+{
+    public class Pokemon
+    {
+        public string Name { get; set; }
+
+        public int Exp { get; set; }
+
+        public bool isPokemon()
+        {
+            return true;
+        }
+    }
+}
---
+++ AlgorithmsDataStructures/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AlgorithmsDataStructures.ADT;
+
+Console.WriteLine("Hello, World!");
+
+Pokemon squirtle = new Pokemon();
